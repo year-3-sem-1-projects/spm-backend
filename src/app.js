80f42fbbc5d23762f71a9aada70a3df0dba6f65f
Sayumi Munasinghe <79864941+SayumiMunasinghe@users.noreply.github.com>
2022-08-20 11:21:41 +0530

    Revert "Post backend done"

--- src/app.js
+++ src/app.js
@@ -1,29 +1,31 @@
-require('dotenv').config()
-import express from 'express'
-import compression from 'compression'
-import helmet from 'helmet'
-import cors from 'cors'
-import connectDB from './database'
-import router from './routes/index.routes'
+require("dotenv").config();
+import express from "express";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import connectDB from "./database";
+import router from "./routes/index.routes";
 import { isCelebrateError } from 'celebrate'
 import { makeResponse } from './utils/response'
-import logger from './utils/logger'
+import logger from "./utils/logger";
 
-const app = express()
+const app = express();
 
-app.use(helmet())
+app.use(helmet());
 
-app.use(compression())
+app.use(compression());
 
-app.use(cors({ origin: true, credentials: true }))
+app.use(cors({ origin: true, credentials: true }));
 
-app.use(express.json({ limit: '1mb' }))
+app.use(express.json({ limit: "1mb" }));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Bashaway Server Up and Running' }))
+app.get("/", (req, res) =>
+  res.status(200).json({ message: "Bashaway Server Up and Running" })
+);
 
-app.use('/api', router)
+app.use("/api", router);
 
 app.use((err, req, res, next) => {
   logger.error(`Error: ${err.message} | Stack: ${err.stack}`)
@@ -34,17 +36,17 @@ app.use((err, req, res, next) => {
   } else if (err.expose) {
     return makeResponse({ res, status: err.status, message: err.message })
   } else
-    return makeResponse({
+    return makeResponse({	
       res,
       status: 500,
       message: "Just patching things up. This'll be over soon!",
     })
 })
 
-connectDB()
+connectDB();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`SPM server successfully started at port ${port}`)
-})
+  console.log(`SPM server successfully started at port ${port}`);
+});
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,8 @@
-import express from 'express'
-import authRouter from './auth.routes'
-import postRouter from './post.routes'
+import express from "express";
+import authRouter from "./auth.routes";
 
-const router = express.Router()
+const router = express.Router();
 
-router.use('/auth', authRouter)
-router.use('/post', postRouter)
+router.use("/auth", authRouter);
 
-export default router
+export default router;
