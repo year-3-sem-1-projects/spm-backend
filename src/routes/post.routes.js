e0a704e65878631763a89badaefc3e594e60426b
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-08-20 13:35:17 +0530

    Post route implementation

---
+++ src/routes/post.routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import controller from '../controllers'
+
+const router = Router()
+
+router.post('/add', controller.insertPost) // insert one post
+router.get('/', controller.getAllPosts) // get all posts
+router.get('/:id', controller.getOnePost) // get one post
+router.put('/:id', controller.updatePost) // update one post
+router.delete('/:id', controller.deletePost) // delete one post
+router.get('/search/:search', controller.searchPosts) // search posts
+// router.get('/popular', controller.getMostPopularPosts) // get most popular posts
+
+export default router
