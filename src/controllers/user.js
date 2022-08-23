1a08841514183f1c60c530cd9c2323ab8084c6a6
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-08-23 11:08:02 +0530

    Add new interests endpoint done

---
+++ src/controllers/user.js
@@ -0,0 +1,15 @@
+import asyncHandler from '../middleware/async'
+import { makeResponse } from '../utils/response'
+import { addInterests } from '../services/user'
+
+export const pickInterests = asyncHandler(async (req, res) => {
+  const result = await addInterests(req.user.email, req.body.interests)
+  if (!result)
+    return makeResponse({
+      res,
+      status: 400,
+      message: 'Cannot add your interests, please try again.',
+    })
+  if (result.status) return makeResponse({ res, ...result })
+  return makeResponse({ res, message: 'Interests Added Successfully!' })
+})
