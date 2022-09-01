27a643841a4fbff7f358ff7b41707890f39d092d
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-09-02 00:08:31 +0530

    Display my questions function added

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { postQuestion, removeQuestion, editQuestion } from '../controllers/question'
+import { postQuestion, removeQuestion, editQuestion, getMyQuestions } from '../controllers/question'
 
 const router = Router()
 
 router.post('/add', postQuestion)
 router.delete('/delete', removeQuestion)
 router.put('/edit', editQuestion)
+router.get('/get/my-questions', getMyQuestions)
 
 export default router
--- src/services/question.js
+++ src/services/question.js
@@ -1,4 +1,4 @@
-import { addOneQuestion, deleteOneQuestion, updateOneQuestion } from "../repository/question"
+import { addOneQuestion, deleteOneQuestion, updateOneQuestion, getMyQuestionsRepository } from "../repository/question"
 
 export const addQuestion = async (questionContent) => {
     const question = await addOneQuestion(questionContent)
@@ -15,11 +15,19 @@ export const deleteQuestion = async (questionId) => {
 }
 
 export const updateQuestion = async (questionContent) => {
-    console.log(questionContent)
+    console.log('questionContent', questionContent)
     const question = await updateOneQuestion({question_id: questionContent.question_id}, questionContent)
     if(!question) return false
     console.log('service', question)
     return question
+}
+
+export const getMyQuestionsService = async (userId) => {
+    console.log('userid', userId)
+    const questions = await getMyQuestionsRepository(userId)
+    if(!questions) return false
+    console.log('service', questions)
+    return questions
 }
 
 
