0006f4b0a5efc8750d99aa50ab631be6f752c885
Lakshika de Zoysa <dezoysalakshika813@gmail.com>
2022-09-09 13:28:22 +0530

    Add new section to Post model

--- src/models/Post.model.js
+++ src/models/Post.model.js
@@ -1,31 +1,29 @@
 const mongoose = require('mongoose')
 
-const PostSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
+const PostSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    viewCount: {
+      type: Number,
+      default: 0,
+    },
   },
-  category: {
-    type: String,
-    required: true,
+  {
+    versionKey: false,
+    timestamps: { createdAt: 'created_at', updatedAt: 'edited_at' },
   },
-  img: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  updatedAt: {
-    type: Date,
-    default: new Date(),
-  },
-  viewCount: {
-    type: Number,
-    default: 0,
-  },
-})
+)
 
 module.exports = mongoose.model('post', PostSchema)
 
