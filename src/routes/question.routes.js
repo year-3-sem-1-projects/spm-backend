40c7a701416cd3e5079f192c50f933bbe3c5c3b7
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-10-21 10:52:16 +0530

    Router Added

--- src/routes/question.routes.js
+++ src/routes/question.routes.js
@@ -10,6 +10,7 @@ router.put('/edit', updateQuestion)
 router.get('/get/my-questions', getMyQuestions)
 router.get('/get/all-questions', getAllQuestions)
 router.get('/get/user-interests/:email', getUserInterests)
+router.get('get/user/:email', getQuestionUser)
 router.get('/get/recommended-questions', getRecommendedQuestions)
 
 export default router
